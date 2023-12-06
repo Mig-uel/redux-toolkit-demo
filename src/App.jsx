e9a6624b6f5302e275eb65fbaab794153d7a4f57
Miguel <35431824+Mig-uel@users.noreply.github.com>
2023-12-05 20:48:02 -0500

    Add Redux functionality to the counter component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,26 @@
+import { useSelector, useDispatch } from 'react-redux'
+import {
+  decrement,
+  increment,
+  incrementByAmount,
+  reset,
+} from './redux/counter.redux'
+
 import './App.css'
 
 function App() {
+  const { count } = useSelector((state) => state.counter)
+  const dispatch = useDispatch()
+
   return (
     <main>
       <h1>Counter</h1>
 
-      <p></p>
-      <button>+</button>
-      <button>-</button>
+      <p>{count}</p>
+      <button onClick={() => dispatch(increment())}>+</button>
+      <button onClick={() => dispatch(decrement())}>-</button>
+      <button onClick={() => dispatch(incrementByAmount(5))}>+5</button>
+      <button onClick={() => dispatch(reset())}>Reset</button>
     </main>
   )
 }
