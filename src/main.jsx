a3ddef9e7fc83d89fb65d401643336afce4f8f56
Miguel <35431824+Mig-uel@users.noreply.github.com>
2023-12-05 20:25:47 -0500

    Add Redux Provider and store to App component

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+
+// redux/global state
+import { Provider } from 'react-redux'
+import store from './redux/store.redux.js'
+
 import App from './App.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
 )
